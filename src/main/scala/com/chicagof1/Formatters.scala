ec28d975fe873c7fd587727045a012e53784a4d8
fcaimi <fedeoasi@gmail.com>
2013-09-01 16:09:26 -0500

    Minor changes.
    * Append csv at the end of filename
    * Use ISO format for dates in filenames

--- src/main/scala/com/chicagof1/Formatters.scala
+++ src/main/scala/com/chicagof1/Formatters.scala
@@ -5,5 +5,5 @@ import org.joda.time.format.DateTimeFormat
 object Formatters {
   val dateFormatter = DateTimeFormat.forPattern("MM/dd/yyyy")
   val timeFormatter = DateTimeFormat.forPattern("hh:mm")
-  val datePrintFormatter = DateTimeFormat.forPattern("MM-dd-yyyy")
+  val datePrintFormatter = DateTimeFormat.forPattern("yyyy-MM-dd")
 }
--- src/main/scala/com/chicagof1/ResultsExporter.scala
+++ src/main/scala/com/chicagof1/ResultsExporter.scala
@@ -7,21 +7,21 @@ import com.github.tototoshi.csv.CSVWriter
 
 object ResultsExporter {
   def writeCsv(fileLocation: String, results: Seq[RacerResult]) {
-    val f = new File(fileLocation)
+    val f = new File(fileLocation  + ".csv")
     val writer = CSVWriter.open(f)
     writer.writeAll(results.map(r => Seq(r.name, r.position, r.time.getStandardSeconds + ":" + r.time.getMillis % 1000)))
   }
 
   def writeCsv(race: Race, baseFolder: String) {
     val fileName = datePrintFormatter.print(race.date) + " - " + timeFormatter.print(race.time)
-    val f = new File(baseFolder + File.separator + fileName)
+    val f = new File(baseFolder + File.separator + fileName + ".csv")
     val writer = CSVWriter.open(f)
     writer.writeAll(race.results.map(r => Seq(r.name, r.position, r.time.getStandardSeconds + ":" + r.time.getMillis % 1000)))
   }
 
   def writeCsv(race: Edition, baseFolder: String) {
     val fileName = datePrintFormatter.print(race.date)
-    val f = new File(baseFolder + File.separator + fileName)
+    val f = new File(baseFolder + File.separator + fileName + ".csv")
     val writer = CSVWriter.open(f)
     writer.writeAll(race.results.map(r => Seq(r.name, r.position, r.time.getStandardSeconds + ":" + r.time.getMillis % 1000)))
   }
