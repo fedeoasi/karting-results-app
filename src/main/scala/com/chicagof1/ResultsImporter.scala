35d0d27f951be0538784c858e496420122fd1f38
fcaimi <fedeoasi@gmail.com>
2013-10-01 18:44:35 -0500

    Create importer for csv result files.

--- src/main/scala/com/chicagof1/ResultsImporter.scala
+++ src/main/scala/com/chicagof1/ResultsImporter.scala
@@ -1,13 +1,13 @@
 package com.chicagof1
 
-import com.chicagof1.model.RacerResult
+import com.chicagof1.model.{Edition, Race, RacerResult}
 import com.github.tototoshi.csv.CSVReader
 import java.io.StringReader
-import org.joda.time.Period
+import org.joda.time.{LocalTime, LocalDate, Period}
 
 object ResultsImporter {
-  def readRacerResult(csvString: String): List[RacerResult] = {
-    val reader = CSVReader.open(new StringReader(csvString))
+  def readRacerResult(filename: String): List[RacerResult] = {
+    val reader = CSVReader.open(new StringReader(filename))
     reader.allWithHeaders().map(r => {
       val timeSplit = r.get("Time").get.split(":")
       RacerResult(
@@ -16,5 +16,19 @@ object ResultsImporter {
         r.get("Kart #").get.toInt,
         Period.seconds(timeSplit(0).toInt).plusMillis(timeSplit(1).toInt).toStandardDuration)
     })
+  }
+
+  def readRace(filename: String, contents: String): Race = {
+    val split = filename.replaceAll(".csv", "").split(" - ")
+    val date = new LocalDate(split(0))
+    val time = new LocalTime(split(1))
+    val results = readRacerResult(contents)
+    Race(date, time, results)
+  }
+
+  def readEdition(filename: String, contents: String): Edition = {
+    val date = new LocalDate(filename.replaceAll(".csv", ""))
+    val results = readRacerResult(contents)
+    Edition(date, results)
   }
 }
--- src/test/scala/com/chicagof1/ResultsImporterSpec.scala
+++ src/test/scala/com/chicagof1/ResultsImporterSpec.scala
@@ -7,16 +7,37 @@ import com.chicagof1.model.RacerResult
 import org.joda.time.Period
 
 class ResultsImporterSpec extends FunSpec with ShouldMatchers {
-  val racerResultCsv = testResourcesDir + "2013-01-23 - 08:48.csv"
-  val raceCsv = testResourcesDir + "2012-11-16.csv"
+  val raceFilename: String = "2013-01-23 - 08:48.csv"
+  val racerResultCsv = testResourcesDir + raceFilename
+  val editionFilename: String = "2012-11-16.csv"
+  val raceCsv = testResourcesDir + editionFilename
 
   describe("A Results Importer") {
-    it("should read a racer result from a csv") {
+    it("should read a racer result from csv contents") {
       val csv = loadFileIntoString(racerResultCsv)
       val result = ResultsImporter.readRacerResult(csv)
       result.size should be(11)
       result(0) should be(RacerResult("Justin", 1, 16, Period.seconds(33).plusMillis(720).toStandardDuration))
       result(10) should be(RacerResult("SuperMario", 11, 8, Period.seconds(39).plusMillis(690).toStandardDuration))
+    }
+
+    it("should read a race from a csv file") {
+      val csv = loadFileIntoString(racerResultCsv)
+      val race = ResultsImporter.readRace(raceFilename, csv)
+      race.date.toString should be("2013-01-23")
+      race.time.toString should be("08:48:00.000")
+      race.results.size should be(11)
+      race.results(0) should be(RacerResult("Justin", 1, 16, Period.seconds(33).plusMillis(720).toStandardDuration))
+      race.results(10) should be(RacerResult("SuperMario", 11, 8, Period.seconds(39).plusMillis(690).toStandardDuration))
+    }
+
+    it("should read an edition from a csv file") {
+      val csv = loadFileIntoString(racerResultCsv)
+      val race = ResultsImporter.readEdition(editionFilename, csv)
+      race.date.toString should be("2012-11-16")
+      race.results.size should be(11)
+      race.results(0) should be(RacerResult("Justin", 1, 16, Period.seconds(33).plusMillis(720).toStandardDuration))
+      race.results(10) should be(RacerResult("SuperMario", 11, 8, Period.seconds(39).plusMillis(690).toStandardDuration))
     }
   }
 }
