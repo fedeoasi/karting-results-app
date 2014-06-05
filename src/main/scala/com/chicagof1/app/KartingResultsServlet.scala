fe8eeea64eeb568400805f0a96d103c853396949
fcaimi <fedeoasi@gmail.com>
2014-06-04 23:25:42 -0500

    Make rendering calls in servlet async using FutureSupport.

--- src/main/scala/com/chicagof1/app/KartingResultsServlet.scala
+++ src/main/scala/com/chicagof1/app/KartingResultsServlet.scala
@@ -4,60 +4,97 @@ import org.json4s.jackson.JsonMethods._
 import org.json4s.JsonDSL._
 import com.chicagof1.data.DataManager
 import com.chicagof1.parsing.VideoSerializer
+import org.scalatra.{AsyncResult, FutureSupport}
+import scala.concurrent.{Future, ExecutionContext}
+import scala.concurrent.ExecutionContext.Implicits.global
 
-class KartingResultsServlet(dataManager: DataManager) extends KartingResultsAppStack {
+class KartingResultsServlet(dataManager: DataManager) extends KartingResultsAppStack with FutureSupport {
   val vs = new VideoSerializer
 
+  override protected implicit def executor: ExecutionContext = global
+
   get("/") {
-    contentType = "text/html"
-    ssp("index")
+    new AsyncResult() {
+      override val is = Future {
+        contentType = "text/html"
+        ssp("index")
+      }
+    }
   }
 
   get("/races/:id") {
-    contentType = "text/html"
-    val raceId = params("id")
-    val race = dataManager.getRaceById(raceId)
-    jade("race", "race" -> race.get)
+    new AsyncResult() {
+      override val is = Future {
+        contentType = "text/html"
+        val raceId = params("id")
+        val race = dataManager.getRaceById(raceId)
+        jade("race", "race" -> race.get)
+      }
+    }
   }
 
   get("/data/races/:id") {
-    contentType = "application/json"
-    val raceId = params("id")
-    val race = dataManager.getRaceById(raceId)
-    val data = race.get.results.map(r => List[String](r.name, r.position.toString, r.kart.toString, r.formattedTime))
-    val json = "aaData" -> data
-    compact(render(json))
+    new AsyncResult() {
+      override val is = Future {
+        contentType = "application/json"
+        val raceId = params("id")
+        val race = dataManager.getRaceById(raceId)
+        val data = race.get.results.map(r => List[String](r.name, r.position.toString, r.kart.toString, r.formattedTime))
+        val json = "aaData" -> data
+        compact(render(json))
+      }
+    }
   }
 
   get("/editions") {
-    contentType = "text/html"
-    val editions = dataManager.editionsWithRaces
-    jade("editions", "editionsWithRaces" -> editions)
+    new AsyncResult() {
+      override val is = Future {
+        contentType = "text/html"
+        val editions = dataManager.editionsWithRaces
+        jade("editions", "editionsWithRaces" -> editions)
+      }
+    }
   }
 
   get("/editions/:id") {
-    contentType = "text/html"
-    val edtionId = params("id")
-    val editionWithRaces = dataManager.getEditionWithRacesById(edtionId)
-    jade("edition", "editionWithRaces" -> editionWithRaces.get)
+    new AsyncResult() {
+      override val is = Future {
+        contentType = "text/html"
+        val edtionId = params("id")
+        val editionWithRaces = dataManager.getEditionWithRacesById(edtionId)
+        jade("edition", "editionWithRaces" -> editionWithRaces.get)
+      }
+    }
   }
 
   get("/data/editions/:id") {
-    contentType = "application/json"
-    val editionId = params("id")
-    val edition = dataManager.getEditionWithRacesById(editionId)
-    val data = edition.get.edition.results.map(r => List[String](r.name, r.position.toString, r.kart.toString, r.formattedTime))
-    val json = "aaData" -> data
-    compact(render(json))
+    new AsyncResult() {
+      override val is = Future {
+        contentType = "application/json"
+        val editionId = params("id")
+        val edition = dataManager.getEditionWithRacesById(editionId)
+        val data = edition.get.edition.results.map(r => List[String](r.name, r.position.toString, r.kart.toString, r.formattedTime))
+        val json = "aaData" -> data
+        compact(render(json))
+      }
+    }
   }
 
   get("/data/videos") {
-    contentType = "application/json"
-    vs.serializeVideos(dataManager.videos)
+    new AsyncResult() {
+      override val is = Future {
+        contentType = "application/json"
+        vs.serializeVideos(dataManager.videos)
+      }
+    }
   }
 
   get("/data/standings") {
-    contentType = "application/json"
-    dataManager.currentChampionship.standings.serialize
+    new AsyncResult() {
+      override val is = Future {
+        contentType = "application/json"
+        dataManager.currentChampionship.standings.serialize
+      }
+    }
   }
 }
